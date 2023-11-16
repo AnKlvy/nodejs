763f062a7143f1b854241bbd857ee52a7256b2bd
AnKlvy <andreyklimov.ver2@gmail.com>
2023-11-16 20:54:41 +0600

    html

--- app.js
+++ app.js
@@ -33,10 +33,47 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
 
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.writeHead(200, { 'Content-Type': 'text/plain;  charset=utf-8' });
 
   res.end('Привет, клиент!');
+
+  fs.readFile('static_page.html', 'utf8', (err, data) => {
+    if (err) {
+      console.error(`Ошибка при чтении файла: ${err.message}`);
+      res.end('Ошибка чтения файла');
+    } else {
+      // Отправка HTML-страницы в ответе
+      res.end(data);
+    }
+  });
 });
+
+const path = require('path');
+
+function readDirectoryContents(directoryPath) {
+  fs.readdir(directoryPath, { withFileTypes: true }, (err, files) => {
+    if (err) {
+      console.error(`Ошибка при чтении директории: ${err.message}`);
+      return;
+    }
+
+    console.log(`Содержимое директории ${directoryPath}:`);
+
+    files.forEach((file) => {
+      const fullPath = path.join(directoryPath, file.name);
+      if (file.isDirectory()) {
+        console.log(`[Поддиректория] ${fullPath}`);
+        readDirectoryContents(fullPath);
+      } else {
+        console.log(`[Файл] ${fullPath}`);
+      }
+    });
+  });
+}
+
+const directoryPath = 'file.txt';
+
+readDirectoryContents(directoryPath);
 
 const port = 8080;
 server.listen(port, () => {
